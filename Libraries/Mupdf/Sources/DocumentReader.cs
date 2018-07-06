6180a3ef3c8737b1567d4c696c8068c2aa147898
clown <tt.clown@gmail.com>
2018-07-06 13:20:09 +0900

    Add project for MuPDF.

---
+++ Libraries/Mupdf/Sources/DocumentReader.cs
@@ -0,0 +1,149 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+/* ------------------------------------------------------------------------- */
+using Cube.FileSystem;
+using System.Diagnostics;
+
+namespace Cube.Pdf.Mupdf.Sources
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// DocumentReader
+    ///
+    /// <summary>
+    /// PDF ファイルを読み込んで各種情報を保持するためのクラスです。
+    /// </summary>
+    ///
+    /// <remarks>
+    /// MuPDF を用いて PDF ファイルの解析を行います。
+    /// </remarks>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class DocumentReader : DocumentReaderBase
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// DocumentReader
+        ///
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /// <param name="src">PDF ファイルのパス</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public DocumentReader(string src) : this(src, string.Empty) { }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// DocumentReader
+        ///
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /// <param name="src">PDF ファイルのパス</param>
+        /// <param name="password">パスワード</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public DocumentReader(string src, string password) :
+            this(src, password, new IO()) { }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// DocumentReader
+        ///
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /// <param name="src">PDF ファイルのパス</param>
+        /// <param name="query">パスワード用オブジェクト</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public DocumentReader(string src, IQuery<string> query) :
+            this(src, query, new IO()) { }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// DocumentReader
+        ///
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /// <param name="src">PDF ファイルのパス</param>
+        /// <param name="password">パスワード</param>
+        /// <param name="io">I/O オブジェクト</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public DocumentReader(string src, string password, IO io) :
+            this(src, new OnceQuery<string>(password), io) { }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// DocumentReader
+        ///
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /// <param name="src">PDF ファイルのパス</param>
+        /// <param name="query">パスワード用オブジェクト</param>
+        /// <param name="io">I/O オブジェクト</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public DocumentReader(string src, IQuery<string> query, IO io) : base(io)
+        {
+            Debug.Assert(io != null);
+
+            // not implemented
+            File        = null;
+            Pages       = null;
+            Metadata    = null;
+            Encryption  = null;
+            Attachments = null;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Dispose
+        ///
+        /// <summary>
+        /// リソースを開放します。
+        /// </summary>
+        ///
+        /// <param name="disposing">
+        /// マネージオブジェクトを開放するかどうか
+        /// </param>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected override void Dispose(bool disposing)
+        {
+            // not implemented
+        }
+
+        #endregion
+    }
+}
